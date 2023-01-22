fde94a5e83243776dbc7ee84f1fe899c3daeb32b
Rémy GRANDIN <remy.gr@ndin.fr>
2023-01-22 23:35:15 +0100

    updated build script

--- build.ps1
+++ build.ps1
@@ -1,13 +1,16 @@
 $tempPath = Join-Path $PSScriptRoot "tmp"
-$buildPath = Join-Path $PSScriptRoot "build"
+$buildPath = Join-Path $PSScriptRoot "dist"
 $iconsPath = Join-Path $buildPath "res" "icons"
+$iconsJSONPath = Join-Path $iconsPath "icons.json"
+
+$srcPath = Join-Path $PSScriptRoot "src"
 
 function DownloadIcons {
     Write-Output "====== Downloading & processing icons ======"
     New-Item -Type Directory -Path $tempPath -Force | Out-Null
     
-    #DownloadAWSIcons
-    DownloadAzureIcons
+    DownloadAWSIcons
+    #DownloadAzureIcons
 }
 
 function DownloadAWSIcons {
@@ -39,34 +42,67 @@ function DownloadAWSIcons {
             FileName = $svgFile.Split("\")[-1]
         }
 
-        $obj.FileName -match "(.*)_(\d\d)_?(Light|Dark)?.svg" | Out-Null
+        if($obj.FullName.Contains("Architecture-Service-Icons"))
+        {
+            $obj.FullName -match "\\Arch_([\da-zA-Z-_]*)\\(?:Arch_)?\d\d\\(?:Arch_ ?(?:Amazon|AWS)?-?)([\da-zA-Z-_]*) ?_(\d\d)_?.svg" | Out-Null
 
-        $obj | Add-Member -MemberType NoteProperty -Name "Size" -Value ([int]($Matches[2]))
-        $obj | Add-Member -MemberType NoteProperty -Name "BaseName" -Value $Matches[1]
-        if ($Matches.count -eq 4) {
-            $obj | Add-Member -MemberType NoteProperty -Name "Name" -Value ("$($obj.BaseName)_$($Matches[3]).svg")
+            $obj | Add-Member -MemberType NoteProperty -Name "Size" -Value ([int]($Matches[3]))
+            $obj | Add-Member -MemberType NoteProperty -Name "BaseName" -Value "AWS_$($Matches[1])_$($Matches[2])"
+            
+            $obj | Add-Member -MemberType NoteProperty -Name "DestName" -Value "$($obj.BaseName).svg"
         }
-        else {
-            $obj | Add-Member -MemberType NoteProperty -Name "Name" -Value ("$($obj.BaseName).svg")
+        elseif ($obj.FullName.Contains("Category-Icons")) {
+            $obj.FullName -match "\\Arch-Category_\d\d\\Arch-([\da-zA-Z-_]*)_(\d\d).svg" | Out-Null
+
+            $obj | Add-Member -MemberType NoteProperty -Name "Size" -Value ([int]($Matches[2]))
+            $obj | Add-Member -MemberType NoteProperty -Name "BaseName" -Value "AWS_$($Matches[1])"
+            
+            $obj | Add-Member -MemberType NoteProperty -Name "DestName" -Value "$($obj.BaseName).svg"
+        }
+        elseif ($obj.FullName.Contains("Res_")) {
+            $obj.FullName -match "\\Res_([\dA-Za-z-_]*)\\[\dA-Za-z-_]*\\Res_(?:AWS|Amazon)?-?([\dA-Za-z-_\.]*) ?_(\d\d)_(Light|Dark).svg" | Out-Null
+
+            $obj | Add-Member -MemberType NoteProperty -Name "Size" -Value ([int]($Matches[3]))
+            $obj | Add-Member -MemberType NoteProperty -Name "BaseName" -Value "AWS_$($Matches[1])_$($Matches[2])_$($Matches[4])"
+            
+            $obj | Add-Member -MemberType NoteProperty -Name "DestName" -Value "$($obj.BaseName).svg"
         }
 
         $svgFilesParsed += $obj
     }
 
-    $svgFilesParsed | Group-Object -Property Name | ForEach-Object {
+    $copiedFiles = @()
+
+    $svgFilesParsed | Group-Object -Property BaseName | ForEach-Object {
         if ($_.Count -eq 1) {
-            Write-Output "    Copy $($_.Group[0].FullName) as $($_.Group[0].Name)  ..."
-            Copy-Item -Path $_.Group[0].FullName -Destination (Join-Path $destPath $_.Name) -Force
+            Write-Output "    Copy $($_.Group[0].FullName) as $($_.Group[0].DestName)  ..."
+            Copy-Item -Path $_.Group[0].FullName -Destination (Join-Path $destPath $_.Group[0].DestName) -Force
         }
         else {
             $maxSize = $_.Group | Measure-Object -Property Size -Maximum | Select-Object -ExpandProperty Maximum
 
             $obj = $_.Group | Where-Object { $_.Size -eq $maxSize }
 
-            Write-Output "    Copy $($obj.FullName) as $($obj.Name)  ..."
-            Copy-Item -Path $obj.FullName -Destination (Join-Path $destPath $_.Name) -Force
+            Write-Output "    Copy $($obj.FullName) as $($obj.DestName)  ..."
+            Copy-Item -Path $obj.FullName -Destination (Join-Path $destPath $obj.DestName) -Force
+        }
+        $copiedFiles += $_.BaseName
+    }
+
+    Write-Output "Adding data to icons.json..."
+    $iconsJson = $null
+    
+    if (test-path $iconsJSONPath) {
+        $iconsJson = Get-Content $iconsJSONPath | ConvertFrom-Json
+    }
+    else {
+        $iconsJson = [PSCustomObject]@{
         }
     }
+    
+    $iconsJson | Add-Member -MemberType NoteProperty -Name "AWS" -Value $copiedFiles -Force | Out-Null
+    $iconsJson | ConvertTo-Json -Depth 100 | Out-File $iconsJSONPath -Force
+
     Write-Output "Done"
 }
 
@@ -112,14 +148,43 @@ function DownloadAzureIcons {
         Copy-Item -Path $svgFileParsed.FullName -Destination (Join-Path $destPath "Azure-$($svgFileParsed.Name)") -Force
     }
 
+    Write-Output "Adding data to icons.json..."
+    $iconsJson = $null
+    
+    if (test-path $iconsJSONPath) {
+        $iconsJson = Get-Content $iconsJSONPath | ConvertFrom-Json
+    }
+    else {
+        $iconsJson = [PSCustomObject]@{
+        }
+    }
+    
+    $iconsJson | Add-Member -MemberType NoteProperty -Name "Azure" -Value $copiedFiles -Force | Out-Null
+    $iconsJson | ConvertTo-Json -Depth 100 | Out-File $iconsJSONPath -Force
+
     Write-Output "Done"
+}
+
+function CopySrc {
+    Write-Output "====== Copying sources content ======"
+    Copy-Item -Path "$srcPath/*" -Destination $buildPath -Recurse -Force
+}
+
+function InitCleanup {
+    Write-Output "====== Cleaning dist ======"
+    Remove-Item -Path $buildPath -Recurse -Force
+}
+
+function EndCleanup {
+    Write-Output "====== Cleaning tmp ======"
+    Remove-Item -Path $tempPath -Recurse -Force
 }
 
 Write-Output "Starting DrawTheNet build process..."
 
+InitCleanup
 DownloadIcons
-
-Write-Output "Cleanup ..."
-#Remove-Item -path $tempPath -recurse -force
+CopySrc
+#EndCleanup
 
 Write-Output "All Done"
